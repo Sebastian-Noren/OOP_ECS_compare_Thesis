f017a8e6818afe0568252dffc884cedd659b6898
Ali Muhammed <Alawi93@live.se>
2021-03-18 11:29:51 +0100

    added person hierarchy and associated classes

---
+++ src/main/java/ecs_bank/models/Banker.java
@@ -0,0 +1,9 @@
+package ecs_bank.models;
+
+public class Banker extends Employee {
+
+
+    public Banker(String firstName, String lastName, String ssn, String registrationDate, String phoneNumber) {
+        super(firstName, lastName, ssn, registrationDate, phoneNumber);
+    }
+}
---
+++ src/main/java/ecs_bank/models/Customer.java
@@ -0,0 +1,58 @@
+package ecs_bank.models;
+
+import java.util.ArrayList;
+
+public class Customer extends Person {
+    private String registrationDate;
+   // private PrivateAccount privateAccount;
+   // private List<SavingsAccount> savingsAccountList;
+    private String phoneNumber;
+
+
+    public Customer(String firstName, String lastName, String ssn, String registrationDate, String phoneNumber) {
+        super(firstName, lastName, ssn);
+        this.registrationDate = registrationDate;
+        this.phoneNumber = phoneNumber;
+
+      // this.List<SavingsAccount> savingsAccountList = new ArrayList<>();
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+    /*
+        public PrivateAccount getPrivateAccount() {
+            return privateAccount;
+        }
+
+        public void setPrivateAccount(PrivateAccount privateAccount) {
+            this.privateAccount = privateAccount;
+        }
+
+        public List<SavingsAccount> getSavingsAccountList() {
+            return savingsAccountList;
+        }
+
+        public void setSavingsAccountList(List<SavingsAccount> savingsAccountList) {
+            this.savingsAccountList = savingsAccountList;
+        }
+    */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    private void transferMoney(String fromAccount, String toAccount){
+
+    }
+    private void deleteSavingsAccount(String accountNbr){
+
+    }
+}
